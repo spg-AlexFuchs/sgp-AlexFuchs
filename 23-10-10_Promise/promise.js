be8af523f663862545554e995c1ec741b55fff05
spg-AlexFuchs <fuc210322@spengergasse.at>
2023-10-10 11:06:16 +0200

    schule

---
+++ 23-10-10_Promise/promise.js
@@ -0,0 +1,11 @@
+console.log("geladen");
+
+function ohneFehler() {
+    if(Math.random()*10-2,5 <=0){
+    console.log(par, "success");
+    }
+}
+function mitfehler(par) {
+    console.log(par, "FAIL");
+}
+x = fetch("http://cats-fact.herokuapp.com/facts").then(successCB);
