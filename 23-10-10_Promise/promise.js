125847da121df3982dbedea97f95fe131c1a5a88
spg-AlexFuchs <fuc210322@spengergasse.at>
2023-10-10 11:24:21 +0200

    school

--- 23-10-10_Promise/promise.js
+++ 23-10-10_Promise/promise.js
@@ -1,9 +1,9 @@
 console.log("geladen");
 
 function ohneFehler(parameter) {
-    console.log(parameter, "success");
+    console.log(parameter, "Funktioniert");
 }
 function mitfehler(parameter) {
-    console.log(parameter,"FAIL");
+    console.log(parameter,"Fehler");
 }
 x = fetch("httpp://cat-fact.herokuapp.com/facts").then(ohneFehler).catch(mitfehler);
