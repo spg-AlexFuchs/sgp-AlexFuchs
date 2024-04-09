06bcbeee03a7bd63d13e698b8d045eda852c47aa
spg-AlexFuchs <fuc210322@spengergasse.at>
2024-04-09 11:12:10 +0200

    erstererererversuchererer

--- 2024-04-02__PLF4_LSG/query.js
+++ 2024-04-02__PLF4_LSG/query.js
@@ -1,2 +1,19 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 console.log('here is my query:');
 // TODO
+async function query(){
+    console.log('querying');
+    let playLists = await prisma.watchlist.findMany({
+        select:{name:true},
+        where:{benutzerid: (3)},
+    });
+
+
+for(let playlist of playLists){
+    console.log(playlist.name);
+}
+}
+console.log("\n");
+
+
