bee93999111156a5e0c271f8119518e342618916
spg-AlexFuchs <fuc210322@spengergasse.at>
2024-06-02 17:50:17 +0200

    bank

--- 2024-05-28__Bank/bastel.js
+++ 2024-05-28__Bank/bastel.js
@@ -1,6 +1,47 @@
-const repl = require('repl');
-const { faker } = require('@faker-js/faker');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const { fakerDE } = require('@faker-js/faker');
 
-const context = { faker };
-const replObj = repl.start();
-Object.assign(replObj.context, context);
+let GEWUENSCHTE_BANKEN = 10;
+let GEWUENSCHTE_CUSTOMERS = 60;
+let GEWUENSCHTE_ACCOUNTS = 25;
+let GEWUENSCHTE_TRANSAKTIONEN = 50;
+
+async function seed(){
+    console.log("seeding Banks");
+    for(d=0; d<GEWUENSCHTE_BANKEN; d++){
+        const bank ={
+            bic: fakerDE.finance.bic(),
+        }
+        await prisma.banks.create({data: bank});
+    }
+
+    console.log("seeding customer and accounts...");
+    
+    for(d=0;d<GEWUENSCHTE_ACCOUNTS; d++){
+        let randomBank = await prisma.banks.findMany({select: {id:true}})
+        const account={
+            iban: fakerDE.finance.iban(),
+            bankID: randomBank[Math.floor(Math.random() * randomBank.length)].id,
+        }
+        await prisma.accounts.create({data:account});
+    }
+
+    for(d=0; d<GEWUENSCHTE_CUSTOMERS; d++){
+        let randomAccount = await prisma.accounts.findMany({select: {id:true}})
+        const customer ={
+            name: fakerDE.person.fullName(),
+            email: fakerDE.internet.email(),
+            accountID: randomAccount[Math.floor(Math.random() * randomAccount.length)].id
+        }
+        await prisma.customers.create({data: customer});
+    }
+
+    for(d=0;d<GEWUENSCHTE_TRANSAKTIONEN;d++){
+        
+    }
+
+
+}
+
+seed();
