5c6b7a7406547aa991b374bd24c9da416f97b8e1
spg-AlexFuchs <fuc210322@spengergasse.at>
2024-06-02 19:43:54 +0200

    seed Finished

--- 2024-05-28__Bank/bastel.js
+++ 2024-05-28__Bank/bastel.js
@@ -16,17 +16,19 @@ async function seed(){
         await prisma.banks.create({data: bank});
     }
 
-    console.log("seeding customer and accounts...");
+    console.log("seeding accounts...");
     
     for(d=0;d<GEWUENSCHTE_ACCOUNTS; d++){
         let randomBank = await prisma.banks.findMany({select: {id:true}})
         const account={
             iban: fakerDE.finance.iban(),
             bankID: randomBank[Math.floor(Math.random() * randomBank.length)].id,
+            kontostand: parseFloat(fakerDE.finance.amount()),
         }
         await prisma.accounts.create({data:account});
     }
 
+    console.log("seeding customers...");
     for(d=0; d<GEWUENSCHTE_CUSTOMERS; d++){
         let randomAccount = await prisma.accounts.findMany({select: {id:true}})
         const customer ={
@@ -37,8 +39,41 @@ async function seed(){
         await prisma.customers.create({data: customer});
     }
 
+    console.log("seeding transactions...");
     for(d=0;d<GEWUENSCHTE_TRANSAKTIONEN;d++){
-        
+        let Accounts = await prisma.accounts.findMany({select: {id:true}});
+        let randomAccount = Accounts[Math.floor(Math.random() * Accounts.length)].id;
+        let randomAccount2 = Accounts[Math.floor(Math.random() * Accounts.length)].id;
+        let randomKontostand = await prisma.accounts.findUnique({
+            select: {kontostand: true},
+            where: {id: randomAccount},
+        });
+        let randomKontostand2 = await prisma.accounts.findUnique({
+            select: {kontostand: true},
+            where: {id: randomAccount2},
+        }); 
+        let ueberweisungsbetrag = parseFloat(Math.floor(Math.random() * randomKontostand.kontostand))
+        const transaktion={
+            verwendungszweck: fakerDE.finance.transactionDescription(),
+            date: fakerDE.date.recent(),
+            amount: ueberweisungsbetrag,
+            fromAccID: randomAccount,
+            toAccID: randomAccount2,
+        }
+        await prisma.accounts.update({
+            where: {id: randomAccount},
+            data: {
+                kontostand: randomKontostand.kontostand-ueberweisungsbetrag,
+            }
+        })
+        await prisma.accounts.update({
+            where: {id: randomAccount2},
+            data: {
+                kontostand: randomKontostand2.kontostand-ueberweisungsbetrag,
+            }
+        })
+
+        await prisma.transactions.create({data: transaktion});
     }
 
 
